bd851f00d3287fc90d5703f331d9a5f638867ff7
faheemkhaaan <faheemkhan.pg@gmail.com>
2024-03-30 12:35:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamberger = document.getElementById('hamberger');
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+const navBar = document.getElementById("navBar");
+
+// navBar.style.transform = 'translateX(300px)'
+
+hamberger.onclick = function(){
+    navBar.classList.remove('hidden')
+    setTimeout(()=>{
+        navBar.classList.remove("translate-x-80");
+    },100);
+}
+closeMenuBtn.onclick = function(){
+    navBar.classList.add("translate-x-80");
+
+    setTimeout(()=>{
+        navBar.classList.add('hidden');
+    },700)
+}
