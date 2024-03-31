56054633d187827e4def02517b6120116029a263
faheemkhaaan <faheemkhan.pg@gmail.com>
2024-03-30 17:38:09 -0700

    Added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation:{
+          slide: 'slide 1s ease-in-out 1 forwards'
+        },
+        keyframes:{
+          slide:{
+            '0%' : {transform:'scale(0)'},
+            '100%' : {transform: 'scale(1)'}
+          }
+        },
+      },
     },
     plugins: [],
   }
